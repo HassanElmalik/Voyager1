b3cd062625b7e1351350cd370a45c94a1dfab35e
Hassan Elmalik <thewaytorealization@gmail.com>
2024-04-23 19:45:48 +0200

    rearange php folders and some other folders

---
+++ ProgrammingWithElzero/php/Assignements/Assignement1-5.php
@@ -0,0 +1,40 @@
+<?php
+/*
+line1
+line2
+line3
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="<?= "UTF-8" ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "My First PHP Page" ?></title>
+  </head>
+  <body>
+    <div><?=  "We Love" ?></div>
+    <div><?=  "Elzero Channel" ?></div>
+  </body>
+</html>
+
+<?php
+//2
+//echo 'Prevent Me From Running Please';
+#echo 'Prevent Me From Running Please';
+/*echo 'Prevent Me From Running Please';*/
+
+//3
+/* My Application
+ Version 1.0
+Created By Elzero
+*/
+
+//4
+
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment
